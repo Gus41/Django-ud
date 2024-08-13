9a0830d201f826fbe9b18b52feaa85af8c7c6e92
Gustavo Damasio <GDOliveira2@ucs.br>
2024-08-13 15:29:22 -0300

    ;.

--- django-basics/recipes/views.py
+++ django-basics/recipes/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
-
+from django.http import HttpRequest
+from utils.main import make_recipe
 # Create your views here.
 
 
 def home(request : HttpRequest):
-    return render(request,'recipes/pages/home.html', context={
-        'name' : 'Teste'
-    })
+    context = {
+        'recipes' : [make_recipe() for _ in range(10)]
+    }
+    return render(request,'recipes/pages/home.html',context)
 
 def recipe(request: HttpRequest, id : int):
-    
-    return render(request,'recipes/pages/recipe-detail.html',context={
-        'teste' : 'teste'
-    })
+    context = {
+        'recipe': make_recipe()
+    }
+    return render(request,'recipes/pages/recipe-detail.html',context)
