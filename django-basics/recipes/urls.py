064621821a9d7249b5387126b696e0dee661aab3
Gustavo <clintgdm@gmail.com>
2024-08-24 19:49:54 -0300

    .

--- django-basics/recipes/urls.py
+++ django-basics/recipes/urls.py
@@ -1,9 +1,10 @@
-from recipes.views import home,recipe
+from recipes.views import home,recipe,category
 from django.urls import path
 
 
 app_name = 'recipe'
 urlpatterns = [
     path('',home,name='home'),
-    path('recipes/<int:id>/',recipe,name='detail')
+    path('recipes/category/<int:id>', category,name='category'),
+    path('recipes/<int:id>/',recipe,name='detail'),
 ]
--- django-basics/recipes/views.py
+++ django-basics/recipes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from utils.main import make_recipe
-from recipes.models import Recipe
+from recipes.models import Recipe,Category
 # Create your views here.
 
 
 def home(request : HttpRequest):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all().filter(is_published=True).order_by('-id')
     context = {
         'recipes' : recipes
     }
@@ -18,3 +18,11 @@ def recipe(request: HttpRequest, id : int):
         'is_detail' : True
     }
     return render(request,'recipes/pages/recipe-detail.html',context)
+
+
+def category( request: HttpRequest, id: int):
+    recipes  = Recipe.objects.filter(is_published=True).filter(category__id=id)
+    context = {
+        'recipes' : recipes
+    }
+    return render(request,'recipes/pages/category.html',context)
