c4e3b0d99987259a2b95117babe9ab1b3f135aa6
Gustavo Damasio <GDOliveira2@ucs.br>
2024-08-27 14:44:21 -0300

    Update settings.py

--- django-basics/project/settings.py
+++ django-basics/project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-=egwu5k)sgcmf4b&0x*zs77%etdx2s&f2+l+10&r719up*yya$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS : list[str] = ['*']
 
 
 
