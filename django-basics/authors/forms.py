9ab20d92d313b8fa9f01802b6c991700e5cab488
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-11-18 17:26:15 -0300

    Update forms.py

--- django-basics/authors/forms.py
+++ django-basics/authors/forms.py
@@ -6,3 +6,23 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','username','email','password']
+
+        labels = {
+            'username' : 'Username',
+            'first_name' : "First name",
+            'last_name' : 'Last name',
+            'email' : 'E-mail',
+            'password' : 'Password'
+        }
+        help_texts = {
+            'email' : 'The e-mail must be valid.'
+        }
+        widgets = { 
+            'first_name' : forms.TextInput(attrs={
+                'placeholder' : 'username here',
+                'class' : 'text-input other-class yadayada'
+            }),
+            'password' : forms.PasswordInput(attrs={
+                'placeholder' : 'type your password here'
+            })
+        }
