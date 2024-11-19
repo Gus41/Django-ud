2f59d9ad5307aa48169f378687d0ffe0690ce8c8
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-11-19 08:52:51 -0300

    Update forms.py

--- django-basics/authors/forms.py
+++ django-basics/authors/forms.py
@@ -1,7 +1,39 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
+
+def add_attr(field,attr_name,attr_val):
+    existing_attr = field.widget.attrs.get(attr_name,'')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_val}'.strip()
+
+
+
+def add_placeholder(field,place_holder_val: str):
+    field.widget.attrs['placeholder'] = place_holder_val.strip()
+
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_attr(self.fields['username'],'placeholder','Type your username here')
+        add_placeholder(self.fields['email'],'Type your email here')
+    
+    
+    
+    password2 = forms.CharField(
+            required=True,
+            widget= forms.PasswordInput(attrs={
+                'placeholder' : 'Repeat your password here'
+            }),
+            error_messages={
+                'required' : 'Password must not be empty'
+            },
+            help_text=(
+                "Passwords must be equals"
+            )
+        )
 
     class Meta:
         model = User
