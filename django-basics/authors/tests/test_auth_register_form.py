af6290e090cbd546ee25d6896d047557fa4ba7df
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-11-22 14:36:49 -0300

    Update test_auth_register_form.py

--- django-basics/authors/tests/test_auth_register_form.py
+++ django-basics/authors/tests/test_auth_register_form.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+from unittest import TestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
 
-
+#this test are using TestCase from unittest, not from django
 
 class AuthRegisterFormUnitTest(TestCase):
     @parameterized.expand([
@@ -13,7 +13,6 @@ class AuthRegisterFormUnitTest(TestCase):
         ('first_name','Type your name here'),
         ('last_name','Type your last name here'),
     ])
-
     def test_placeholders_are_correct(self,field : str, placeholder_expected: str):
         form = RegisterForm() 
         placeholder = form[field].field.widget.attrs['placeholder']
@@ -24,7 +23,6 @@ class AuthRegisterFormUnitTest(TestCase):
     @parameterized.expand([
         ('email','The e-mail must be valid'),
     ])
-
     def test_helptexts_are_correct(self,field : str, help_text_nedded: str):
         form = RegisterForm() 
         help_text  = form[field].field.help_text
@@ -38,7 +36,6 @@ class AuthRegisterFormUnitTest(TestCase):
             ('email', 'E-mail'),
             ('password', 'Password'),
     ])
-
     def test_labels_are_correct(self,field: str, label_nedded: str):
         form = RegisterForm() 
         label  = form[field].field.label
