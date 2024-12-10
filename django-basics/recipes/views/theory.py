c47904b9c42e0044a7da6d664aa43527a9878f5b
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-12-10 11:26:39 -0300

    q()

--- django-basics/recipes/views/theory.py
+++ django-basics/recipes/views/theory.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from recipes.models import Recipe
+from django.db.models import Q
+
 
 def Theory(request, *args, **kwargs):
-    recipes = Recipe.objects.all()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains='da',
+              id__gt=2,
+              is_published=True,) |
+            Q(
+                id__gt=1000
+            )
+        )
+    )[:10]
     
     context = {
         'recipes': recipes
