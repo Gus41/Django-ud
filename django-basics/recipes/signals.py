1d4969ad5fbadd114daa71843971fabf204549c7
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2024-12-11 16:27:47 -0300

    Update signals.py

--- django-basics/recipes/signals.py
+++ django-basics/recipes/signals.py
@@ -1,28 +1,34 @@
-from recipes.models import Recipe 
-from django.db.models.signals import pre_delete,pre_save
-from django.dispatch import receiver
 import os
-
-
+ 
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+ 
+from recipes.models import Recipe
+ 
+ 
 def delete_cover(instance):
     try:
         os.remove(instance.cover.path)
-        print("Removing cover from recipe")
-    except(ValueError,FileNotFoundError):
-        print("Error removing cover path")
-
-
-@receiver(pre_delete,sender=Recipe)
-def delete_recipe_cover(sender,instance,*args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
-    delete_cover(old_instance)
-    
-
-@receiver(pre_save,sender=Recipe)
-def delete_recipe_old_cover(sender,instance,*args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
-    new_cover = old_instance.cover != instance.cover
-    if new_cover:
+    except (ValueError, FileNotFoundError):
+        ...
+ 
+ 
+@receiver(pre_delete, sender=Recipe)
+def recipe_cover_delete(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+ 
+    if old_instance:
         delete_cover(old_instance)
-    
-
+ 
+ 
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+ 
+    if not old_instance:
+        return
+ 
+    is_new_cover = old_instance.cover != instance.cover
+ 
+    if is_new_cover:
+        delete_cover(old_instance)
