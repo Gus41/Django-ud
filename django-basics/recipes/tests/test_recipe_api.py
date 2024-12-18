d7f48505bad883bde007e46c7168a4aae1e7fe7d
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-12-18 13:55:41 -0300

    Update test_recipe_api.py

--- django-basics/recipes/tests/test_recipe_api.py
+++ django-basics/recipes/tests/test_recipe_api.py
@@ -1,4 +1,5 @@
-from unicodedata import category
+from tarfile import data_filter
+from urllib import response
 from django.urls import reverse
 from rest_framework import test
 from recipes.tests.test_recipe_base import RecipeMixin
@@ -7,6 +8,20 @@ from unittest.mock import patch
 
 
 class RecipeApiTest(test.APITestCase,RecipeMixin):
+
+    def get_auth_data(self):
+        userdata={
+            'username': 'user',
+            'password': 'password123'
+        }
+        user = self.make_author(username=userdata.get("username"),password=userdata.get("password"))
+
+        response = self.client.post(path=reverse('recipe:token_obtain_pair'),data=userdata)
+        
+        return {
+            **response.data,
+            "user": user
+        }
     
     def test_recipe_api_list_returns_200(self):
         api_url = reverse("recipe:recipe_api-list")
@@ -57,3 +72,69 @@ class RecipeApiTest(test.APITestCase,RecipeMixin):
             len(response.data.get("results")),
             4
         )
+    def test_recipe_api_list_user_must_send_jwt_to_create_recipe(self):
+        api_url = reverse("recipe:recipe_api-list")
+        response = self.client.post(api_url)
+        self.assertEqual(response.status_code,401)
+
+
+    def test_recipe_api_logged_users_can_create_recipe(self):
+        data={
+            "title":"This is the title",
+            "description": "description",
+            "preparation_time":1,
+            "preparation_time_unit": "Minute",
+            "servings":1,
+            "servings_unit":"Person",
+            "preparation_steps": "preparation steps"
+        }
+
+        response = self.client.post(
+            reverse("recipe:recipe_api-list"),
+            data=data,
+            HTTP_AUTHORIZATION=f'Bearer {self.get_auth_data().get("access")}'
+            )
+        
+        self.assertEqual(
+            response.status_code,
+            201
+        )
+
+
+    def test_recipe_api_logged_users_can_update_recipe(self):
+        auth_data = self.get_auth_data()
+        recipe = self.make_recipe()
+        recipe.author = auth_data.get("user")
+        recipe.save()
+
+
+        new_title = "Diferent Title 123"
+        response = self.client.patch(
+            reverse("recipe:recipe_api-detail",args=(recipe.id,)),
+            data={"title":new_title},
+            HTTP_AUTHORIZATION=f'Bearer {auth_data.get("access")}'
+            )
+
+        self.assertEqual(
+            response.data.get("title"),
+            new_title
+        )
+
+
+    def test_recipe_api_logged_users_cant_update_owned_by_another_user(self):
+        auth_data = self.get_auth_data()
+        recipe = self.make_recipe()
+        recipe.save()
+
+
+        new_title = "Diferent Title 123"
+        response = self.client.patch(
+            reverse("recipe:recipe_api-detail",args=(recipe.id,)),
+            data={"title":new_title},
+            HTTP_AUTHORIZATION=f'Bearer {auth_data.get("access")}'
+            )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )    
